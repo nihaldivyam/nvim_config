42441b7db45d7dccbbce68339cdfd6229b7b678a
Divyam  Azad <nihaldivyam@gmail.com>
2025-07-05 03:42:28 +0530

    install treesitter

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
-
+vim.g.mapleader = " "
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -29,6 +29,20 @@ local plugins = {
     end
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master', lazy = false, build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true }
+        })
+	end
+  },
+  {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = {
@@ -37,7 +51,8 @@ local plugins = {
     },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<space>fd', builtin.find_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   }
 }
